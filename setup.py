5d00ca9cd16028689c9d7409ec85f414cb7dae3a
yzj2019 <1223358821@qq.com>
2025-04-20 05:19:25 +0800

    使用pip安装

--- setup.py
+++ setup.py
@@ -16,31 +16,13 @@ sources = [
     if file.endswith(".cpp") or file.endswith(".cu")
 ]
 
-# def get_python_paths():
-#     from distutils.sysconfig import get_python_inc, get_config_var
-#     return {
-#         'include_dirs': [get_python_inc()],
-#         'library_dirs': [get_config_var('LIBDIR')],
-#     }
-# python_paths = get_python_paths()
-
-
-
 setup(
-    name="scannet_segmentator",
-    version="1.0",
-    install_requires=["torch", "numpy", "numba"],
-    packages=["scannet_segmentator"],
-    package_dir={"scannet_segmentator": "functions"},
     ext_modules=[
         CppExtension(
             name='libsegmentator._C',
             sources=sources,
-            extra_compile_args=['-std=c++14'],
+            extra_compile_args=['-std=c++17'],
         )
     ],
-    # include_dirs=[*python_paths['include_dirs']],
     cmdclass={"build_ext": BuildExtension},
 )
-
-# include_dirs 什么时候需要加?
